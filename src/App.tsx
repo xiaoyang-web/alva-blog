bb4f83e10f99391445818c73aff1ab47ffcdf9ff
alva-yky <alva-yky@xm430.com>
2022-04-15 17:37:28 +0800

    build: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 const App = () => {
-  return <div className='app'>{`alva-yky's blog`}</div>
+  return <div className='app px-2'>{`alva-yky's blog`}</div>
 }
 
 export default App
